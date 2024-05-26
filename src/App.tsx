6a3afcef953a6b6d3cf01dbcf2927cff9d382985
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-05-26 19:21:13 +0500

    removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import Header from './components/Header';
 import HeroSection from './components/home/HeroSection';
 import FeaturesSection from './components/home/FeaturesSection';
-import Container from './components/Container';
 import GallerySection from './components/home/GallerySection';
 import { EmblaCarousel } from './components/home/EmblaCarousel';
-import { Link } from 'react-router-dom';
 import Footer from './components/Footer';
 
 function App() {
