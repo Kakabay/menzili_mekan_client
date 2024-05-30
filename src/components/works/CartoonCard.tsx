70099c500d89b1c8219f6abbd62dfa211cc009e7
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-05-30 17:00:03 +0500

    cartoons card

---
+++ src/components/works/CartoonCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CartoonCard = () => {
+  return (
+    <div className="flex flex-col gap-[16px]">
+      <div className="max-h-[161px] w-full">
+        <img src="/cover.png" alt="cover" />
+      </div>
+      <h4 className="text-[14px] font-medium leading-[19.6px] tracking-[-1%]">
+        Guild of Guardians eSports Announcement
+      </h4>
+    </div>
+  );
+};
+
+export default CartoonCard;
