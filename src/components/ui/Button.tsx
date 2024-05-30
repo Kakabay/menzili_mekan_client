be388c119289d8c091dfb8d097f92ecc1c51f6e4
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-05-30 17:00:22 +0500

    button made abstract

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import clsx from 'clsx';
 
-const Button = () => {
+type IProps = {
+  text: string;
+  type: 'primary' | 'secondary';
+};
+
+const Button = ({ text, type }: IProps) => {
   return (
-    <button className="uppercase text-[18px] tracking-[1%] leading-[24.3px] bg-blue text-center px-[32px] py-[16px] rounded-full">
-      showreel
+    <button
+      className={clsx(
+        ' tracking-[1%] text-center rounded-full',
+        {
+          'uppercase text-[18px] px-[32px] py-[16px] leading-[24.3px] bg-blue': type === 'primary',
+        },
+        {
+          'uppercase text-[16px] px-[24px] py-[12px] leading-[21.6px] border border-white':
+            type === 'secondary',
+        },
+      )}>
+      {text}
     </button>
   );
 };
