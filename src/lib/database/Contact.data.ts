a51ffe37e2a633e8353c69bc4002e3f9bb1ecc24
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-02 18:50:18 +0500

    data for contact page

---
+++ src/lib/database/Contact.data.ts
@@ -0,0 +1,23 @@
+export const contactCardData = [
+  {
+    path: '/contact/1.svg',
+    title: 'Production',
+    text: `
+    For information on business opportunities and manufacturing services, please contact our office
+    `,
+  },
+  {
+    path: '/contact/2.svg',
+    title: 'Recruitment',
+    text: `
+    Our creative team is constantly growing, join us by filling out an application form in our office
+    `,
+  },
+  {
+    path: '/contact/3.svg',
+    title: 'Our office',
+    text: `
+    Level 2, Block A3, An Binh City, 232 Pham Van Dong, Bac Tu Liem, Hanoi, Vietnam
+    `,
+  },
+];
