78f7c94ba8a4d753ae8ac2b672b2e729933996fb
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-02 18:45:18 +0500

    added shadow for header

--- tailwind.config.js
+++ tailwind.config.js
@@ -80,6 +80,9 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      dropShadow: {
+        headerShadow: '0 2px 8px rgba(0, 0, 0, 0.10)',
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
