b283e97c5950ed759bc25418e57ef1ba47b13eba
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-02 19:01:40 +0500

    added close icon

--- src/components/home/YoutubeWindow.tsx
+++ src/components/home/YoutubeWindow.tsx
@@ -1,3 +1,4 @@
+import { useZusHome } from '@/zustand/useZusHome';
 import { motion } from 'framer-motion';
 import { useEffect } from 'react';
 import YouTube, { YouTubePlayer, YouTubeProps } from 'react-youtube';
@@ -8,6 +9,8 @@ export const YoutubeWindow = () => {
 
     return () => document.body.classList.remove('overflow-hidden');
   }, []);
+
+  const setActiveVideo = useZusHome((state) => state.setActiveVideo);
 
   const opts: YouTubeProps['opts'] = {
     height: '100%',
@@ -28,8 +31,16 @@ export const YoutubeWindow = () => {
       exit={{ y: '-100%' }}
       transition={{ ease: 'linear' }}
       className="fixed top-0 right-0 bottom-0 left-0 z-[3000] px-5 overflow-hidden w-full h-screen bg-black">
+      <img
+        width={30}
+        height={30}
+        src="https://flaticons.net/icon.php?slug_category=mobile-application&slug_icon=close"
+        alt="close"
+        onClick={() => setActiveVideo(false)}
+        className="w-[30px] h-[30px] cursor-pointer absolute top-[50px] right-[80px] z-[5000]"
+      />
       <YouTube
-        className="w-full h-full"
+        className="w-full h-full cursor-default"
         videoId="https://www.youtube.com/watch?v=HIAcT5roxWY"
         opts={opts}
         onReady={onPlayerReady}
