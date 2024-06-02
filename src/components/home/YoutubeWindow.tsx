e89396526961d31f12a3b2d5d498f7dcc3b1b46c
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-02 18:49:30 +0500

    data for cards on contact page

--- src/components/home/YoutubeWindow.tsx
+++ src/components/home/YoutubeWindow.tsx
@@ -1,15 +1,39 @@
-import YouTube from "react-youtube";
+import { motion } from 'framer-motion';
+import { useEffect } from 'react';
+import YouTube, { YouTubePlayer, YouTubeProps } from 'react-youtube';
 
-const YoutubeWindow = ({ path }: { path: string }) => {
-  const opts = {
-    height: "390",
-    width: "640",
+export const YoutubeWindow = () => {
+  useEffect(() => {
+    document.body.classList.add('overflow-hidden');
+
+    return () => document.body.classList.remove('overflow-hidden');
+  }, []);
+
+  const opts: YouTubeProps['opts'] = {
+    height: '100%',
+    width: '100%',
     playerVars: {
       autoplay: 1,
     },
   };
 
-  return <YouTube videoId={path} opts={opts} />;
-};
+  function onPlayerReady(event: { target: YouTubePlayer }) {
+    event.target.pauseVideo();
+  }
 
-export default YoutubeWindow;
+  return (
+    <motion.div
+      initial={{ y: '-100%' }}
+      animate={{ y: 0 }}
+      exit={{ y: '-100%' }}
+      transition={{ ease: 'linear' }}
+      className="fixed top-0 right-0 bottom-0 left-0 z-[3000] px-5 overflow-hidden w-full h-screen bg-black">
+      <YouTube
+        className="w-full h-full"
+        videoId="https://www.youtube.com/watch?v=HIAcT5roxWY"
+        opts={opts}
+        onReady={onPlayerReady}
+      />
+    </motion.div>
+  );
+};
