18b170154738a491f2105cefb42326e72438fcd5
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-02 18:45:39 +0500

    remove uneccessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import FeaturesSection from './components/home/FeaturesSection';
-import GallerySection from './components/home/GallerySection';
-import { EmblaCarousel } from './components/PartnersSlider';
 import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
 
