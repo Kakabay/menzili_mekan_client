5139c6c075d3c4811d4bbe709ed8d6032313cc4a
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-06-10 17:18:37 +0500

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home.tsx';
-import Works from './pages/Works.tsx';
-import Services from './pages/Services.tsx';
-import Contact from './pages/Contact.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import Works from "./pages/Works.tsx";
+import Services from "./pages/Services.tsx";
+import Contact from "./pages/Contact.tsx";
+import Project from "./pages/Project.tsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path: '/works',
+        path: "/works",
         element: <Works />,
       },
       {
-        path: '/services',
+        path: "/services",
         element: <Services />,
       },
       {
-        path: '/contact',
+        path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/project",
+        element: <Project />,
       },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
---
+++ src/pages/Project.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "@/components/HeroSection";
+
+const Project = () => {
+  return (
+    <div>
+      {/* <HeroSection size="big" page="project" banner="/project/cover.png" /> */}
+    </div>
+  );
+};
+
+export default Project;
