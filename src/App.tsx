5be80369d51270c75d48eb946c6137ed4ba7c149
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-07-01 17:08:54 +0500

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
-import useFavicon from './hooks/useFavicon';
+// import useFavicon from './hooks/useFavicon';
 
 const App: React.FC = () => {
   // useFavicon();
